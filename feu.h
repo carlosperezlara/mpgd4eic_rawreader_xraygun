0dcf3739f5d9537b3459c8a1d97822402e540218
Alexander Kiselev <ayk@bnl.gov>
2020-01-16 11:34:30 -0600

    extra

---
+++ feu.h
@@ -0,0 +1,10 @@
+#ifndef __FEU_H__
+#define __FEU_H__
+
+#include <pmonitor/pmonitor.h>
+#include <Event/Event.h>
+#include <Event/EventTypes.h>
+
+int process_event (Event *e); //++CINT 
+
+#endif /* __FEU_H__ */
